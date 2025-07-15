bc687a92602f98146c50eca9645ea7fc97b35211
rauf-alluvium <intern@novusha.com>
2025-07-15 14:09:11 +0530

    fix: update CORS allowedHeaders to include user-related fields

--- server/app.js
+++ server/app.js
@@ -36,7 +36,10 @@ app.use(
     ], // Your React app's URL
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", 'user-id',
+        'username',
+        'user-role',
+        'x-username'],
   })
 );
 
